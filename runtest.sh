cdf787107a99e6a3556ed63e8f9c9779dd45afda
Jim Nelson <jnelson@archive.org>
2018-08-13 10:40:04 +0900

    runtest.sh: Exit script early if test fails

--- runtest.sh
+++ runtest.sh
@@ -8,4 +8,8 @@ fi
 for ut in `find -s test -name "$SEARCH" -or -name "$SEARCH.php" -or -name "${SEARCH}Test.php"`; do
   echo; echo $ut:
   vendor/phpunit/phpunit/phpunit --bootstrap test/Did/Test/Bootstrap.php $ut
+  EC=$?
+  if [ $EC -ne "0" ]; then
+    exit $EC
+  fi
 done
